d968d61397eeec89e2724cf26bdbe2ec4d6bda1b
vinay kumar yadav <vinku920@gmail.com>
2022-02-11 16:32:51 +0530

    Database and migrations

--- blog/views.py
+++ blog/views.py
@@ -1,9 +1,30 @@
+from multiprocessing import context
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Post
+
+# posts = [
+#     {
+#         'author': 'Vinay',
+#         'title': 'Blog Post 1',
+#         'content' : 'First post content',
+#         'date_posted': 'Feb 10, 2022'
+#     },
+#     {
+#         'author': 'Kumar',
+#         'title': 'Blog Post 2',
+#         'content' : 'Second post content',
+#         'date_posted': 'March 10, 2022'
+#     }
+# ]
 
 def home(request):
-    return HttpResponse('<h1>Blog Home </h1>')
-def home1(request):
-    return HttpResponse('<h1>Blog Home1 </h1>')
+    context={
+        'posts': Post.objects.all()
+    }
+    return render(request, 'blog/home.html', context)
+
 def about(request):
-    return HttpResponse('<h1>Blog About </h1>')
+    # return HttpResponse('<h1>Blog About </h1>')
+    return render(request, 'blog/about.html', {'title': 'About'})
+
