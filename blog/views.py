7c50f4a74ac8054343265b1853f4a9cb6988320e
vinay kumar yadav <vinku920@gmail.com>
2022-02-15 13:17:33 +0530

    Succesfully Tested Reset Emailid

--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,3 @@
-from multiprocessing import context
 from django.http import HttpResponse
 from django.shortcuts import render, get_object_or_404
 from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
--- users/views.py
+++ users/views.py
@@ -1,6 +1,4 @@
-from multiprocessing import context
 from django.shortcuts import render, redirect
-# from django.contrib.auth.forms import UserCreationForm
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
 from .forms import UserRegisterForm, UserUpdateForm, ProfileUpdateForm
